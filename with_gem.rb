73afebfa3b19aa9aa986f7635109d0e81429e2f2
Fernanda Thomazinho <fernanda.thomazinho@codus.com.br>
2018-02-05 10:34:35 -0200

    Finished with_gem.rb script

--- with_gem.rb
+++ with_gem.rb
@@ -1,13 +1,18 @@
 # using SendGrid's Ruby Library
 # https://github.com/sendgrid/sendgrid-ruby
 require 'sendgrid-ruby'
+require 'dotenv/load'
 include SendGrid
 
-from = Email.new(email: 'fernanda.thomazinho@codus.com.br')
-to = Email.new(email: 'fernanda.thomazinho@codus.com.br')
-subject = 'Sending with SendGrid is Fun'
-content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-mail = Mail.new(from, subject, to, content)
+mail = Mail.new
+mail.from = Email.new(email: 'fernanda.thomazinho@codus.com.br')
+mail.subject = 'Bem-vindo(a)'
+personalization = Personalization.new
+personalization.add_to(Email.new(email: 'fernanda.thomazinho@codus.com.br'))
+personalization.add_substitution(Substitution.new(key: '<%name%>', value: 'Fernanda'))
+personalization.add_substitution(Substitution.new(key: '<%id%>', value: '123'))
+mail.add_personalization(personalization)
+mail.template_id = '3234e19e-d56b-4298-83c2-8d0b74570322'
 
 sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 response = sg.client.mail._('send').post(request_body: mail.to_json)
